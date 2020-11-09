9d4767a3913fb311b8f6e1fd4e69e13c6c58fef0
timothy <masikotimo@gmail.com>
2020-11-09 15:51:14 +0300

    edited sth small

--- tests/test_post_requests.py
+++ tests/test_post_requests.py
@@ -6,10 +6,13 @@ from flask import g
 from flask import Flask
 
 
-
-
-
 def test_register_page_accepts_post_request(client):
     rsp = client.post('/register', data={"todo_text": "do something useful"})
+    assert rsp.status == '200 OK'
+    assert '<form' in rsp.get_data(as_text=True)
+
+
+def test_login_page_accepts_post_request(client):
+    rsp = client.post('/login', data={"todo_text": "do something useful"})
     assert rsp.status == '200 OK'
     assert '<form' in rsp.get_data(as_text=True)
