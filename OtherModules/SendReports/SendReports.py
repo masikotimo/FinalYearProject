580a6a2911af81e7a4ce8ccb7d305403de77cc63
timothy <masikotimo@gmail.com>
2020-11-29 05:17:48 +0300

    working before integration

--- OtherModules/SendReports/SendReports.py
+++ OtherModules/SendReports/SendReports.py
@@ -5,6 +5,7 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import os
 
 subject = "System Reports "
 body = "This is an email with attachment sent from Python"
@@ -22,6 +23,7 @@ message["Bcc"] = receiver_email  # Recommended for mass emails
 # Add body to email
 message.attach(MIMEText(body, "plain"))
 
+
 filename = "employee.pdf"  # In same directory as script
 
 # Open PDF file in binary mode
@@ -35,17 +37,22 @@ with open(filename, "rb") as attachment:
 encoders.encode_base64(part)
 
 # Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {filename}",
-)
+
+part.add_header('Content-Disposition', 'attachment', filename=filename)
 
 # Add attachment to message and convert message to string
 message.attach(part)
 text = message.as_string()
 
 # Log in to server using secure context and send email
+# context = ssl.create_default_context()
+# with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+#     server.login(sender_email, password)
+#     server.sendmail(sender_email, receiver_email, text)
+
+
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, text)
+server = smtplib.SMTP_SSL("smtp.gmail.com", 465 )
+server.login(sender_email, password)
+server.sendmail(sender_email, receiver_email, text)
+server.quit()
